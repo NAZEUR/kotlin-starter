3e77231233856faf8a7b0f54d014f34f2a396f86
NAZEUR <nabilanurhusnayap@gmail.com>
2024-09-18 13:23:21 +0700

    menambahkan cara untuk memanggil properti dengan nama yang sama di kelas outer class

--- src/House.kt
+++ src/House.kt
@@ -15,3 +15,21 @@ fun main() {
     val room = house.Room()
     room.measureRoomArea()
 }
+
+/*
+class House {
+    // Properti atau anggota outer class
+    val buildingArea = 100
+    val totalRooms = 4
+
+    inner class Room {
+        // Properti atau anggota inner class
+        val totalRooms = 4
+        fun measureRoomArea() {
+            // Inner class dapat mengakses properti atau anggota outer class
+            print(buildingArea/this@House.totalRooms)
+        }
+    }
+}
+
+ */
