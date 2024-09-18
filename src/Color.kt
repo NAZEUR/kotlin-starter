facfabc4979b83e25cb43466cf831a284eee9e54
NAZEUR <nabilanurhusnayap@gmail.com>
2024-09-18 14:01:01 +0700

    menambahkan kelas result menggunakan sealed class

---
+++ src/Color.kt
@@ -0,0 +1,31 @@
+fun main(){
+    val colors: Array<Color> = enumValues()
+    colors.forEach {color ->
+        println(color)
+    }
+
+    val color: Color = enumValueOf("RED")
+    println("Color is $color")
+
+
+}
+
+enum class Color(val value: Int) {
+    RED(0xFF0000){
+        override fun printValue() {
+            println("value of RED is $value")
+        }
+    },
+    GREEN(0x00FF00){
+        override fun printValue() {
+            println("value of GREEN is $value")
+        }
+    },
+    BLUE(0x0000FF){
+        override fun printValue() {
+            println("value of BLUE is $value")
+        }
+    };
+
+    abstract fun printValue()
+}
---
+++ src/Result.kt
@@ -0,0 +1,20 @@
+sealed class Result {
+    data class Success(val data: Any) : Result()
+    data class Error(val message: String) : Result()
+    object Loading : Result()
+}
+
+fun main() {
+    val result: Result = Result.Error("Oops!")
+    when (result) {
+        is Result.Success -> {
+            println("Success: ${result.data}")
+        }
+        is Result.Error -> {
+            println("Error: ${result.message}")
+        }
+        is Result.Loading -> {
+            println("Loading...")
+        }
+    }
+}
